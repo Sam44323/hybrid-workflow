7d177e06bb8f41b4bc5cb892fe80b3f91000464b
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-26 02:10:58 +0530

    add: added custom article-meta for values

--- hackernews-search/pages/article/[articleId].tsx
+++ hackernews-search/pages/article/[articleId].tsx
@@ -2,9 +2,21 @@ import React from "react";
 import { NextPage } from "next";
 
 import styles from "./Article.module.scss";
+import Head from "next/head";
 
 const Article: NextPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Head>
+        <title>Article</title>
+        <meta
+          name="HackerNews Search"
+          content="A GUI for searching hackernews blogs using their API's"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
 };
 
 export default Article;

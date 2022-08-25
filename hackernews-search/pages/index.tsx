73f3073405bc0d7bbbcb3b3dab8ce735bdd27af9
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-26 00:10:17 +0530

    add: added custom flexer

--- hackernews-search/pages/index.tsx
+++ hackernews-search/pages/index.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable react/no-children-prop */
+import React from "react";
+import { BsSearch } from "react-icons/bs";
 import styles from "./Home.module.scss";
 import type { NextPage } from "next";
+import { Button, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import Head from "next/head";
 
 const Home: NextPage = () => {
+  const [searchInput, setSearchInput] = React.useState<string>("");
   return (
     <>
       <Head>
@@ -15,6 +20,15 @@ const Home: NextPage = () => {
       </Head>
       <main className={styles.HomeContainer}>
         <h1>Hackernews Search</h1>
+        <section className={styles.InputContainer}>
+          <InputGroup>
+            <InputLeftElement pointerEvents="none" children={<BsSearch />} />
+            <Input type="text" placeholder="news title" />
+          </InputGroup>
+          <Button className={styles.Button} colorScheme="green" variant="solid">
+            Search
+          </Button>
+        </section>
       </main>
     </>
   );

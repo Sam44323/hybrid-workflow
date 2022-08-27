5c24b41cd347c62ac13c5922e3c917a55d8aa1ca
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-27 23:08:41 +0530

    add: added code for seller-fetchers

--- ecomm-backend/controllers/buyer.controllers.ts
+++ ecomm-backend/controllers/buyer.controllers.ts
@@ -1,8 +1,23 @@
 import { Request, Response } from 'express'
 import SellerModel from '../models/sellers.model'
 import OrdersModel from '../models/orders.model'
+import { ObjectId } from 'mongodb'
 
-const getSellersList = async (req: Request, res: Response) => {}
+const getSellersList = async (req: Request, res: Response) => {
+  try {
+    const sellers = await SellerModel.find()
+    if (!sellers) {
+      return res.status(404).json({
+        message: 'Sellers not found'
+      })
+    }
+    return res.status(200).json({ sellers })
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message
+    })
+  }
+}
 
 const getSellerCatalogue = async (req: Request, res: Response) => {}
 

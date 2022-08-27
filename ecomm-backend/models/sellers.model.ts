e074823811d7e4a55510c542859fb6d7c5b66407
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-27 20:20:55 +0530

    add: added code for product-models

--- ecomm-backend/models/sellers.model.ts
+++ ecomm-backend/models/sellers.model.ts
@@ -7,6 +7,9 @@ class Sellers {
 
   @prop({ required: true, type: String })
   orders: string
+
+  @prop({ required: true, type: mongoose.Schema.Types.ObjectId, default: [] })
+  catalogue: mongoose.Schema.Types.ObjectId[]
 }
 
 const SellerModel = getModelForClass(Sellers)

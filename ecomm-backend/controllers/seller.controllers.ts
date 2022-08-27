04ab1462a0965fa2efcc8e05f0e8882fd15eb9e9
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-27 22:52:54 +0530

    add: added batch updaters for prod-ids

--- ecomm-backend/controllers/seller.controllers.ts
+++ ecomm-backend/controllers/seller.controllers.ts
@@ -3,33 +3,41 @@ import ProductModel from '../models/products.model'
 import OrdersModel from '../models/orders.model'
 import SellerModel from '../models/sellers.model'
 import mongoose from 'mongoose'
+import { ObjectId, ObjectID } from 'bson'
 
 const createCatalog = async (req: Request, res: Response) => {
   const { sellerId, data } = req.body
   if (!sellerId || data.length === 0) {
+    console.log(sellerId)
+    console.log(data)
     return res.status(400).json({ message: 'Payload incomplete...' })
   }
   try {
-    const seller = await SellerModel.findById(sellerId)
+    const seller = await SellerModel.findById(new ObjectID(sellerId))
+
     if (!seller) {
       return res.status(400).json({ message: 'Seller not found' })
     }
-    const prodIds: mongoose.Schema.Types.ObjectId[] = null
+    const prodIds = []
 
-    Promise.all(
+    await Promise.all(
       data.map(async (product: { name: string; price: number }) => {
         const existingProduct = await ProductModel.find({ name: product.name })
-        if (existingProduct) {
+        if (existingProduct.length > 0) {
           return
         }
         const prod = await ProductModel.create(product)
         prodIds.push(prod._id)
+        console.log('Prod: ', prod._id)
       })
     )
 
+    console.log('ProdIds: ', prodIds)
+
     seller.catalogue = [...seller.catalogue, ...prodIds]
 
-    await seller.save()
+    await SellerModel.findOneAndUpdate({ _id: new ObjectID(sellerId) }, seller)
+
     return res.status(201).json({ message: 'Catalogue created...' })
   } catch (err) {
     console.log(err)
--- ecomm-backend/utils/auth.middleware.ts
+++ ecomm-backend/utils/auth.middleware.ts
@@ -3,16 +3,14 @@ import jwt from 'jsonwebtoken'
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization
+
   if (!authHeader) {
     return res.status(401).json({ message: 'No token provided.' })
   }
-  const parts = authHeader.split(' ')
-  if (parts.length !== 2) {
-    return res.status(401).json({ message: 'Token error.' })
-  }
-  const [scheme, token] = parts
-  jwt.verify(token, process.env.JWT_SECRET, (err: jwt.VerifyErrors, _) => {
+
+  jwt.verify(authHeader, process.env.JWT_SECRET, (err: jwt.VerifyErrors, _) => {
     if (err) {
+      console.log(err)
       return res.status(401).json({ message: 'Token invalid.' })
     }
     next()

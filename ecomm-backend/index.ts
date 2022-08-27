a20576bb6f545675b399d87100a835efc42c677b
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-27 20:10:24 +0530

    add: added loggers with envs

---
+++ ecomm-backend/index.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response, NextFunction } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import helmet from 'helmet'
+import mongoose from 'mongoose'
+import Logger from './utils/logger'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI, {
+  // @ts-ignore
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const connection = mongoose.connection // connection from mongoose
+// Once connection established
+connection.once('open', () => {
+  Logger.info('✅ MongoDB connection established successfully!')
+})
+
+// If error while connection
+connection.on('error', (err: any) => {
+  Logger.info('❌ Failed to connect to DB on startup ' + err.message)
+})
+// When the connection is disconnected
+connection.on('disconnected', () => {
+  Logger.info('🔌 Mongoose default connection to DB disconnected')
+})
+
+const app = express()
+
+app.use(
+  (
+    error: { code: any; message: any },
+    _req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    Logger.info(`
+    [${new Date().toLocaleString()}] : ❌ Error`)
+    if (res.headersSent) {
+      return next(error)
+    }
+    if (res.headersSent) {
+      return
+    }
+    res.status(error.code || 500)
+    res.json({ message: error.message || 'An unknown error occurred!' })
+  }
+)
+
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+  Logger.info(`✅ Backend is running on PORT: ${port}`)
+})
